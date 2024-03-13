4510d9519cd550f02aa6938bcecc8deb4390c26a
Shubham Deshpande <shubham14deshpande@gmail.com>
2024-03-13 22:31:54 +0530

    Configure React app for deployment to GitHub Pages

--- src/Components/IPL_Players.js
+++ src/Components/IPL_Players.js
@@ -17,73 +17,73 @@ export default function IPLPlayers() {
         <>
             <div className="outline">
                 <div className='inner-outline'>
-                    <div className='break'>
-                        <div className='head'>
+                    <div className='break-mi'>
+                        <div className='head-mi'>
                             <img src={MI} className='img-logo' alt='MI' />
-                            <p className='team_name'>Mumbai Indians</p>
+                            <p className='team_name_mi'>Mumbai Indians</p>
                         </div>
                         <TeamInput teamName='MI' />
                     </div>
-                    <div className='break'>
-                        <div className='head'>
+                    <div className='break-csk'>
+                        <div className='head-csk'>
                             <img src={CSK} className='img-logo' alt='CSK' />
-                            <p className='team_name'>Chennai Super Kings</p>
+                            <p className='team_name_csk'>Chennai Super Kings</p>
                         </div>
                         <TeamInput teamName='CSK' />
                     </div>
-                    <div className='break'>
-                        <div className='head'>
+                    <div className='break-rcb'>
+                        <div className='head-rcb'>
                             <img src={RCB} className='img-logo' alt='RCB' />
-                            <p className='team_name_RCB'>Royal Challengers Bangalore</p>
+                            <p className='team_name_rcb'>Royal Challengers Bangalore</p>
                         </div>
                         <TeamInput teamName='RCB' />
                     </div>
-                    <div className='break'>
-                        <div className='head'>
+                    <div className='break-lsg'>
+                        <div className='head-lsg'>
                             <img src={LSG} className='img-logo' alt='LSG' />
-                            <p className='team_name'>Lucknow Super Giants</p>
+                            <p className='team_name_lsg'>Lucknow Super Giants</p>
                         </div>
                         <TeamInput teamName='LSG' />
                     </div>
-                    <div className='break'>
-                        <div className='head'>
+                    <div className='break-gt'>
+                        <div className='head-gt'>
                             <img src={GT} className='img-logo' alt='GT' />
-                            <p className='team_name'>Gujarat Titans</p>
+                            <p className='team_name_gt'>Gujarat Titans</p>
                         </div>
                         <TeamInput teamName='GT' />
                     </div>
-                    <div className='break'>
-                        <div className='head'>
+                    <div className='break-dc'>
+                        <div className='head-dc'>
                             <img src={DC} className='img-logo' alt='DC' />
-                            <p className='team_name'>Delhi Capitals</p>
+                            <p className='team_name_dc'>Delhi Capitals</p>
                         </div>
                         <TeamInput teamName='DC' />
                     </div>
-                    <div className='break'>
-                        <div className='head'>
+                    <div className='break-kkr'>
+                        <div className='head-kkr'>
                             <img src={KKR} className='img-logo' alt='KKR' />
-                            <p className='team_name'>Kolakata Knight Riders</p>
+                            <p className='team_name_kkr'>Kolkata Knight Riders</p>
                         </div>
                         <TeamInput teamName='KKR' />
                     </div>
-                    <div className='break'>
-                        <div className='head'>
+                    <div className='break-rr'>
+                        <div className='head-rr'>
                             <img src={RR} className='img-logo' alt='RR' />
-                            <p className='team_name'>Rajasthan Royals</p>
+                            <p className='team_name_rr'>Rajasthan Royals</p>
                         </div>
                         <TeamInput teamName='RR' />
                     </div>
-                    <div className='break'>
-                        <div className='head'>
+                    <div className='break-pbks'>
+                        <div className='head-pbks'>
                             <img src={PBKS} className='img-logo' alt='PBKS' />
-                            <p className='team_name'>Punjab Kings</p>
+                            <p className='team_name_pbks'>Punjab Kings</p>
                         </div>
                         <TeamInput teamName='PBKS' />
                     </div>
-                    <div className='break'>
-                        <div className='head'>
+                    <div className='break-srh'>
+                        <div className='head-srh'>
                             <img src={SRH} className='img-logo' alt='SRH' />
-                            <p className='team_name'>Sunrisers Hyderabad</p>
+                            <p className='team_name_srh'>Sunrisers Hyderabad</p>
                         </div>
                         <TeamInput teamName='SRH' />
                     </div>
--- src/Components/Team_Input.js
+++ src/Components/Team_Input.js
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from 'react';
 
 export default function TeamInput({ teamName }) {
     const initialUserState = {
-        cost1: 0.0, cost2: 0.0, cost3: 0.0, cost4: 0.0, cost5: 0.0, cost6: 0.0,
-        name1: "", name2: "", name3: "", name4: "", name5: "", name6: ""
+        cost1: 0.0, cost2: 0.0, cost3: 0.0, cost4: 0.0, cost5: 0.0, cost6: 0.0, cost7: 0.0, cost8: 0.0,
+        name1: "", name2: "", name3: "", name4: "", name5: "", name6: "", name7: "", name8: ""
     };
 
     const initialCostState = {
-        cost7: 20.0
+        cost9: 20.0
     };
 
     const [user, setUser] = useState(initialUserState);
@@ -29,10 +29,10 @@ export default function TeamInput({ teamName }) {
         const { name, value } = e.target;
         setUser(prevState => {
             const updatedUser = { ...prevState, [name]: parseFloat(value) };
-            const remainingCost = 20.0 - (updatedUser.cost1 + updatedUser.cost2 + updatedUser.cost3 + updatedUser.cost4 + updatedUser.cost5 + updatedUser.cost6);
-            setCost({ ...cost, cost7: remainingCost });
+            const remainingCost = 20.0 - (updatedUser.cost1 + updatedUser.cost2 + updatedUser.cost3 + updatedUser.cost4 + updatedUser.cost5 + updatedUser.cost6 + updatedUser.cost7 + updatedUser.cost8);
+            setCost({ ...cost, cost9: remainingCost });
             localStorage.setItem(`user_${teamName}`, JSON.stringify(updatedUser));
-            localStorage.setItem(`cost_${teamName}`, JSON.stringify({ ...cost, cost7: remainingCost }));
+            localStorage.setItem(`cost_${teamName}`, JSON.stringify({ ...cost, cost9: remainingCost }));
             return updatedUser;
         });
     };
@@ -69,31 +69,39 @@ export default function TeamInput({ teamName }) {
         <>
             <div className='textbox'>
                 <input type='text' className='name_input' name='name1' placeholder='Purchased By' value={user.name1} onChange={handleNameInputs} />
-                <input type='number' className='input' name='cost6' placeholder='Cost' value={user.cost6} onChange={handleCostInputs} />
-            </div>
-            <div className='textbox'>
-                <input type='text' className='name_input' name='name2' placeholder='Player Name' value={user.name2} onChange={handleNameInputs} />
                 <input type='number' className='input' name='cost1' placeholder='Cost' value={user.cost1} onChange={handleCostInputs} />
             </div>
             <div className='textbox'>
-                <input type='text' className='name_input' name='name3' placeholder='Player Name' value={user.name3} onChange={handleNameInputs} />
+                <input type='text' className='name_input' name='name2' placeholder='Player Name' value={user.name2} onChange={handleNameInputs} />
                 <input type='number' className='input' name='cost2' placeholder='Cost' value={user.cost2} onChange={handleCostInputs} />
             </div>
             <div className='textbox'>
-                <input type='text' className='name_input' name='name4' placeholder='Player Name' value={user.name4} onChange={handleNameInputs} />
+                <input type='text' className='name_input' name='name3' placeholder='Player Name' value={user.name3} onChange={handleNameInputs} />
                 <input type='number' className='input' name='cost3' placeholder='Cost' value={user.cost3} onChange={handleCostInputs} />
             </div>
             <div className='textbox'>
-                <input type='text' className='name_input' name='name5' placeholder='Player Name' value={user.name5} onChange={handleNameInputs} />
+                <input type='text' className='name_input' name='name4' placeholder='Player Name' value={user.name4} onChange={handleNameInputs} />
                 <input type='number' className='input' name='cost4' placeholder='Cost' value={user.cost4} onChange={handleCostInputs} />
             </div>
             <div className='textbox'>
-                <input type='text' className='name_input' name='name6' placeholder='Player Name' value={user.name6} onChange={handleNameInputs} />
+                <input type='text' className='name_input' name='name5' placeholder='Player Name' value={user.name5} onChange={handleNameInputs} />
                 <input type='number' className='input' name='cost5' placeholder='Cost' value={user.cost5} onChange={handleCostInputs} />
             </div>
             <div className='textbox'>
+                <input type='text' className='name_input' name='name6' placeholder='Player Name' value={user.name6} onChange={handleNameInputs} />
+                <input type='number' className='input' name='cost6' placeholder='Cost' value={user.cost6} onChange={handleCostInputs} />
+            </div>
+            <div className='textbox'>
+                <input type='text' className='name_input' name='name7' placeholder='Player Name' value={user.name7} onChange={handleNameInputs} />
+                <input type='number' className='input' name='cost7' placeholder='Cost' value={user.cost7} onChange={handleCostInputs} />
+            </div>
+            <div className='textbox'>
+                <input type='text' className='name_input' name='name8' placeholder='Player Name' value={user.name8} onChange={handleNameInputs} />
+                <input type='number' className='input' name='cost8' placeholder='Cost' value={user.cost8} onChange={handleCostInputs} />
+            </div>
+            <div className='textbox'>
                 <p className='purse_text'>Remaining Purse : </p>
-                <input type='number' className='input' placeholder='Cost' name='cost7' value={cost.cost7} readOnly />
+                <input type='number' className='input' placeholder='Cost' name='cost9' value={cost.cost9} readOnly />
             </div>
             <button className='button' onClick={handleReset}>Reset Values</button>
             <button className='button' onClick={handleRollback}>Rollback</button>
